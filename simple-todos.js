5b4ddd14e56a546e8e76c88d13f04393f5865ed4
topleft <pete.james.jeffryes@gmail.com>
2016-01-06 14:48:40 -0700

    worked with session

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,16 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
@@ -21,11 +30,26 @@ if (Meteor.isClient) {
         text: text,
         createdAt: new Date() // current time
       });
- 
       // Clear form
       event.target.text.value = "";
+  },
+  "change .hide-completed input": function (event) {
+    Session.set("hideCompleted", event.target.checked);
+  }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
     }
   });
+
 }
 
 if (Meteor.isServer) {
